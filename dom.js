d73b886cb8f688c5bff12c2c6392dfe25e555c61
sonalighodake43772 <sonalighodake867>
2022-09-15 18:29:59 +0530

    clssbyname

---
+++ dom.js
@@ -0,0 +1,68 @@
+/*console.log(document);
+console.log(document.domain);
+console.log(document.URL);
+ console.log(document.doctype);
+ console.log(document.title);
+//  document.title='items';
+ console.log(document.head);
+ console.log(document.body);
+ console.log(document.all);
+ console.log(document.all[10]);
+//  document.all[10].textContent="lists"
+ console.log(document.forms);
+ console.log(document.links);
+ console.log(document.images);
+
+//  getelementbyID
+// change title
+
+// console.log(document.getElementById('items'));
+//  console.log(document.getElementById('header-title'));
+var it=document.getElementById('items');
+console.log(it);
+var headertitle=document.getElementById('header-title');
+var header=document.getElementById('main-header');
+console.log(headertitle);
+headertitle.textContent='hello';
+headertitle.innerText='basic list';
+headertitle.innerHTML='<h3>basic list</h3>';
+header.style.borderBottom='solid 3px black';
+
+// change add item
+
+var title=document.getElementById('title');
+console.log(title);
+title.style.fontWeight='bold';
+title.style.color='green';*/
+
+// get element byclassname
+
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+// items[1].textContent='hello';
+
+// items[1].style.fontWeight='bold';
+// items[1].style.backgroundColor='red';
+items[2].style.backgroundColor='green';
+for(var i=0;i<items.length;i++)
+{
+    items[i].style.fontWeight='bold';
+}   
+
+// for(var i=0;i<items.length;i++)
+// {
+    
+//     items[i].style.backgroundColor='yellow';
+// }
+
+
+// gives error
+// items.style.backgroundColor='red';
+
+
+
+
+
+ 
+ 
