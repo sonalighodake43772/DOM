d92c0976aa86b492da5d847e070d051ae0129a9f
sonalighodake43772 <sonalighodake867>
2022-09-22 11:31:45 +0530

    scaleupmoreuser

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // loocalstorage
 // localStorage.setItem('name','sonali');
 // localStorage.getItem('name');
- // localStorage.removeItem('name');
+// localStorage.removeItem('name');
 
 //  sessionstorage
 /*sessionStorage.setItem('name','fogg');
@@ -16,36 +16,71 @@ document.cookie='name=john';*/
 const myForm = document.querySelector('#my-form');
 const namein = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
+const userList = document.querySelector('#users');
+
 
 myForm.addEventListener('submit', onSubmit);
 
 
+
 function onSubmit(e) {
   e.preventDefault();
+
+
   // instead of console add name and email to the localstorage
 
   // localStorage.setItem('name',namein.value);
   // localStorage.setItem('email',emailInput.value);
 
 
-// add name and email to localstorage as a object
+  // add name and email to localstorage as a object
 
-let userdetails=
+  let userdetails =
   {
-    name:namein.value,
-    email:emailInput.value
+    name: namein.value,
+    email: emailInput.value
 
 
   };
-   localStorage.setItem(' userdetails',JSON.stringify(userdetails));
+  // using this we can add only one user and when we add new user older will removed
+  //  localStorage.setItem('userdetails',JSON.stringify(userdetails));
 
-  if(namein.value==''||emailInput.value=='')
-  {
+  // using uniqe id we can add multiple user older will nt removed
+  localStorage.setItem(userdetails.email, JSON.stringify(userdetails));
 
-     msg.classList.add('error');     /** it is used to add class or id from stylesheet */
-    // alert('please enter all field');
-    msg.innerHTML='please enter all field';
-  }
+  ShowUser(userdetails);
 }
 
-  
+// show user on screen
+window.addEventListener("DOMContentLoaded", () => {
+  const localStorageObj = localStorage;
+  const localstoragekey = Object.keys(localStorageObj)
+
+  for (var i = 0; i < localstoragekey.length; i++) {
+    const key = localstoragekey[i]
+    const userString = localStorageObj[key];
+    const userObj = JSON.parse(userString);
+    ShowUser(userObj);
+  }
+})
+
+
+
+
+
+function ShowUser(user) {
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(`${user.name}: ${user.email}`));
+  userList.appendChild(li);
+
+
+  // const parentnode=document.getElementById('users');
+  // const childnode=`<li>${user.name}:${user.email}</li>`
+  // parentnode.innerHTML=parentnode.innerHTML + childnode;
+  // Clear fields
+  namein.value = '';
+  emailInput.value = '';
+
+}
+
+
